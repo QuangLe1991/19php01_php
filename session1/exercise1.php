072e77b7ae0c3f7804a1e11e78551bfebdc40b79
unknown <lequangdn91@gmail.com>
2019-05-30 20:28:50 +0700

    nop bai tap

--- session1/exercise1.php
+++ session1/exercise1.php
@@ -8,7 +8,13 @@
 		for ($j = 0; $j <= $budget/$n; $j++) { 
 			for ($k = 0; $k <= $budget/$p; $k++) { 
 				if (($i*$m + $j*$n + $k*$p) == $budget) {
-					echo "$i to 10.000, $j to 20.000, $k to 50.000";
+					if (($i == 0) && ($j == 0)) echo "$k to 50.000";
+					else if (($i == 0) && ($k == 0)) echo "$j to 20.000";
+					else if (($j == 0) && ($k == 0)) echo "$i to 10.000";
+					else if ($i == 0) echo "$j to 20.000, $k to 50.000";
+					else if ($j == 0) echo "$i to 10.000, $k to 50.000";
+					else if ($k == 0) echo "$i to 10.000, $j to 20.000";
+					else echo "$i to 10.000, $j to 20.000, $k to 50.000";
 					echo "<br>";
 				}
 			}
