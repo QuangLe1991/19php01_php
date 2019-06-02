6a03bd75e0abf9ae8fb2fab29223f232e9814257
unknown <lequangdn91@gmail.com>
2019-06-02 12:22:01 +0700

    update bai tap

--- session2/example2.php
+++ session2/example2.php
@@ -13,50 +13,60 @@
 	<?php 
 		//Validate fields
 		$errName = $errEmail = $errPhoneNumber = $errGender = $errHomeland = $errBirthday = '';
-		if (isset($_REQUEST['register'])) {
-			if ($_REQUEST['name'] == '') {
+		if (isset($_GET['register'])) {
+			if ($_GET['name'] == '') {
 				$errName = 'Please input name';
 			} else {
 				$errName = '';
 			}
-			if ($_REQUEST['email'] == '') {
+			if ($_GET['email'] == '') {
 				$errEmail = 'Please input email';
 			} else {
 				$errEmail = '';
 			}
-			if ($_REQUEST['phonenumber'] == '') {
+			if ($_GET['phonenumber'] == '') {
 				$errPhoneNumber = 'Please input phone number';
-			} else if (!is_numeric($_REQUEST['phonenumber'])) {
+			} else if (!is_numeric($_GET['phonenumber'])) {
 				$errPhoneNumber = 'Please input number not string';
 			} else {
 				$errPhoneNumber = '';
 			}
-			if (!isset($_REQUEST['gender'])) {
+			if (!isset($_GET['gender'])) {
 					$errGender = 'Please input gender';
 			} else {
 					$errGender = '';
 			}
-			if ($_REQUEST['homeland'] == '') {
+			if ($_GET['homeland'] == '') {
 				$errHomeland = 'Please input home land';
 			} else {
 				$errHomeland = '';
 			}
-			if ($_REQUEST['birthday'] == '') {
+			if ($_GET['birthday'] == '') {
 				$errBirthday = 'Please input birthday';
 			} else {
 				$errBirthday = '';
 			}				
 
-	//Save data for retain after submitting
-		$name 			= $_REQUEST['name'];
-		$email 			= $_REQUEST['email'];
-		$phonenumber 	= $_REQUEST['phonenumber'];
-		$birthday 		= $_REQUEST['birthday'];
-		$homeland 		= $_REQUEST['homeland'];
-	}
+		//Save data for retain after submitting
+			$name 			= $_GET['name'];
+			$email 			= $_GET['email'];
+			$phonenumber 	= $_GET['phonenumber'];
+			$birthday 		= $_GET['birthday'];
+		
+		//Print form
+			echo 'Name : '.$_GET['name']; echo "<br>";
+			echo 'Email :'.$_GET['email']; echo "<br>";
+			echo 'Phone Number : '.$_GET['phonenumber']; echo "<br>";
+			if (isset($_GET['gender'])) {
+				echo 'Gender : '.$_GET['gender']; 
+				echo "<br>";
+			}
+			echo 'Homeland : '.$_GET['homeland']; echo "<br>";
+			echo 'Birthday : '.$_GET['birthday']; echo "<br>";
+		}
 	?>
-	<h1>Example 1 form</h1>
-	<form action="#" name="Example2Form" method="REQUEST">
+	<h1>Example 2 form</h1>
+	<form action="#" name="Example2Form" method="GET">
 		<p>Name :
 			<input type="text" name="name" value="<?php if (isset($name)) echo "$name";?>">
 			<p class="error"><?php echo $errName; ?></p>
@@ -70,24 +80,24 @@
 			<p class="error"><?php echo $errPhoneNumber; ?></p>
 		</p>
 		<p>Gender :
-			<input type="radio" name="gender" value="male" 
+			<input type="radio" name="gender" value="Male" 
 				<?php 
-					if (isset($_REQUEST['gender'])) { 
-						if ($_REQUEST['gender'] == 'male') echo"checked=\"checked\"";
+					if (isset($_GET['gender'])) { 
+						if ($_GET['gender'] == 'Male') echo"checked=\"checked\"";
 					}
 				?>
-			> Male<br>
-			<input type="radio" name="gender" value="female"
+			> Male
+			<input type="radio" name="gender" value="Female"
 				<?php 
-					if (isset($_REQUEST['gender'])) { 
-						if ($_REQUEST['gender'] == 'female') echo"checked=\"checked\"";
+					if (isset($_GET['gender'])) { 
+						if ($_GET['gender'] == 'Female') echo"checked=\"checked\"";
 					}
 				?>
-			> Female<br>
-			<input type="radio" name="gender" value="other"
+			> Female
+			<input type="radio" name="gender" value="Other"
 				<?php 
-					if (isset($_REQUEST['gender'])) { 
-						if ($_REQUEST['gender'] == 'other') echo"checked=\"checked\"";
+					if (isset($_GET['gender'])) { 
+						if ($_GET['gender'] == 'Other') echo"checked=\"checked\"";
 					}
 				?>
 			> Other
@@ -96,24 +106,24 @@
 		<p>Homeland :
 			<select name="homeland">
 				<option value=""></option>
-				<option value="hanoi"
+				<option value="Ha Noi"
 					<?php 
-						if (isset($_REQUEST['homeland'])) { 
-							if ($_REQUEST['homeland'] == 'hanoi') echo "selected=\"selected\"";
+						if (isset($_GET['homeland'])) { 
+							if ($_GET['homeland'] == 'Ha Noi') echo "selected=\"selected\"";
 						}
 					?>>Ha Noi
 				</option>
-				<option value="danang" 
+				<option value="Da Nang" 
 					<?php 
-						if (isset($_REQUEST['homeland'])) { 
-							if ($_REQUEST['homeland'] == 'danang') echo "selected=\"selected\"";
+						if (isset($_GET['homeland'])) { 
+							if ($_GET['homeland'] == 'Da Nang') echo "selected=\"selected\"";
 						}
 					?>>Da Nang
 				</option>
-				<option value="hochiminh"
+				<option value="Ho Chi Minh"
 					<?php 
-						if (isset($_REQUEST['homeland'])) { 
-							if ($_REQUEST['homeland'] == 'hochiminh') echo "selected=\"selected\"";
+						if (isset($_GET['homeland'])) { 
+							if ($_GET['homeland'] == 'Ho Chi Minh') echo "selected=\"selected\"";
 						}
 					?>>Ho Chi Minh
 				</option>
@@ -121,11 +131,7 @@
 			<p class="error"><?php echo $errHomeland; ?></p>
 		</p>
 		<p>Birth Day :
-			<input type="date" name="birthday" value="
-				<?php 
-					if (isset($birthday)) echo "$birthday"; 
-				?>"
-			>
+			<input type="date" name="birthday" value="<?php if (isset($birthday)) echo "$birthday";?>">
 			<p class="error"><?php echo $errBirthday; ?></p>
 		</p>
 		<p>
